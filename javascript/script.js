b78463c0c31bd2fe4c25c643ede72dd771584c52
sophiekinoko <sophiekinoko@gmail.com>
2023-06-20 16:26:02 +0200

    add medusims in work + fixes

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-//Slider
+
 			// Listen for click on the document
 		document.addEventListener('click', function (event) {
 		  
@@ -27,27 +27,3 @@
 		  // Toggle our content
 		  content.classList.toggle('active');
 		})
-
-
-
-
-//Button scroll back to top
-		//Get the button:
-		mybutton = document.getElementById("myBtnTop");
-
-		// When the user scrolls down 20px from the top of the document, show the button
-		window.onscroll = function() {scrollFunction()};
-
-		function scrollFunction() {
-		  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			mybutton.style.display = "block";
-		  } else {
-			mybutton.style.display = "none";
-		  }
-		}
-
-		// When the user clicks on the button, scroll to the top of the document
-		function topFunction() {
-		  document.body.scrollTop = 0; // For Safari
-		  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-		}
